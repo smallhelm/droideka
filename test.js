4f007c208b5073e392f88a49b5552fca336f4742
Matthew Wright <matthew@smallhelm.com>
2016-05-14 10:43:12 -0600

    fixing tests

--- test.js
+++ test.js
@@ -1,26 +1,8 @@
 var _ = require('lodash');
+var test = require('tape');
 var droideka = require('./index');
 
 var n_tests_to_run = 1000;
-
-var n_failed = 0;
-var n_passed = 0;
-
-var assertRecoded = function(text){
-  var e = droideka.encode(text);
-  eval(droideka.js_code_decode);
-  var decoded = d(e);
-  if(decoded === text){
-    n_passed++;
-  }else{
-    console.error("failed to recode", text, "but instead got back", decoded);
-    n_failed++;
-  }
-};
-
-//
-//Let's do some generative testing!!!
-//
 
 var randomChar = (function(){
   var chars = "\"'~!@#$%^&*()<:>[]{}.,+=-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split("");
@@ -32,33 +14,28 @@ var randomString = function(){
   return _.map(_.range(1, _.random(0, 10000)), randomChar).join("");
 };
 
-_.each(_.range(0, n_tests_to_run), function(){
-  assertRecoded(randomString());
+test('encoding then decoding strings', function(t){
+  _.each(_.range(0, n_tests_to_run), function(){
+    var text = randomString();
+    var encoded = droideka.encode(text);
+    eval(droideka.js_code_decode);
+    var decoded = d(encoded);
+    t.notEquals(text, encoded);
+    t.equals(text, decoded);
+  });
+  t.end();
 });
-//test seeds
-(function(){
-  var text = 'some consistent string';
 
-    droideka.encode(text, 'seed 1'),
-    droideka.encode(text, 'seed 2')
-  var encoded = [
-    droideka.encode(text),
-    droideka.encode(text, null),
-    droideka.encode(text, undefined),
-  ];
-
-  if(_.size(encoded) === _.size(_.unique(encoded))){
-    n_passed++;
-  }else{
-    console.error("failed to recode", text, "but instead got back", decoded);
-    n_failed++;
-  }
-}());
-
-console.log("Done");
-console.log("#failed:", n_failed);
-console.log("#passed:", n_passed);
-
-if(n_failed > 0){
-  process.exit(1);
-}
+test('seeded encoding', function(t){
+  var text = 'hello world';
+  t.equals(
+    droideka.encode(text, 'seed?'),
+    "Z3.k+u*@]D)Qwfs{A'06S%(a&gX2t9^xCdRI5cJHmjL[NhyO7<T}Uo:1$YFEMrvp\"#nP>elq~,W-VGKbiz84!=B$izz\" a\"~zN"
+  );
+  console.log(JSON.stringify(droideka.encode(text, 'something else')));
+  t.equals(
+    droideka.encode(text, 'something else'),
+    "){j^6=PZX1<B-V.3n!D%a2O#*iNv[fb4whtF5Kgl~dLCuk9UT@R7WrImY>:xpJ'+yMG\"HAoSe,&z80}]EQc($qsu(RRQ CQyRW"
+  );
+  t.end();
+});
